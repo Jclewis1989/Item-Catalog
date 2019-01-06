ac003e9f2f6c3ce12497220548c9a149edcfae7b
James Crane Lewis III <James.Crane.Lewis.III@gmail.com>
2019-01-06 05:45:11 -0800

    Create show route for all items to display

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/items')
+def showAll():
+    items = session.query(Item).all()
+    # Returning every item in the catalog to perform crud operations on
+    return render_template('items.html')
+
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
