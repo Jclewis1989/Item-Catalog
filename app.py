c28f31bbb01234dc1b517ddc884799873c079d13
James Crane Lewis III <James.Crane.Lewis.III@gmail.com>
2019-01-11 18:01:35 -0800

    Create JSON routes

--- app.py
+++ app.py
@@ -12,12 +12,25 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Serialize JSON
+# Entire catalog item and details in JSON
 @app.route('/items/<int:item_id>/category/JSON')
-def itemCategoryJSON(item_id):
+def itemJSON(item_id):
     items = session.query(Item).filter_by(id=item_id).one()
     itemCategories = session.query(ItemCategory).filter_by(
         item_id=item_id).all()
-    return jsonify(itemCategories=[i.serialize for i in items])
+    return jsonify(itemCategories=[i.serialize for i in itemCategories])
+
+# Every detail of an item category in JSON
+@app.route('/items/<int:item_id>/details/<int:details_id>/JSON')
+def itemDetailsJSON(item_id, details_id):
+    itemDetails = session.query(ItemCategory).filter_by(id=details_id).one()
+    return jsonify(itemDetails=itemDetails.serialize)
+
+# Every item category in JSON
+@app.route('/items/JSON')
+def itemOnlyJSON():
+    items = session.query(Item).all()
+    return jsonify(items=[i.serialize for i in items])
 
 # Index route to home page
 @app.route('/')
@@ -118,7 +131,7 @@ def editDetails(item_id, itemDetails_id):
 def deleteDetails(item_id, itemDetails_id):
     itemToDelete = session.query(ItemCategory).filter_by(id=item_id).one()
     item = session.query(Item).filter_by(id=item_id).one()
-    if request.method == 'POST': # Post must come from a form, not href! Fix later today
+    if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
         return redirect(url_for('showItem', item_id=item_id))
