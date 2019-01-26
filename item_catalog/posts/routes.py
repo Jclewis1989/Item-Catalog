abeef5e0dfc0caa63c268f5553f50085fb4c2673
James Crane Lewis III <James.Crane.Lewis.III@gmail.com>
2019-01-26 07:50:16 -0800

    Adjust posts. functions

--- item_catalog/posts/routes.py
+++ item_catalog/posts/routes.py
@@ -28,29 +28,29 @@ def newItem():
         session.add(newItem)
         session.commit()
         session.rollback() #Running into errors if this code is not present
-        return redirect(url_for('main.showAll'))
+        return redirect(url_for('posts.showAll'))
     else:
         return render_template('new.html')
 
 # Edit route to edit item names
 @posts.route('/items/<int:item_id>/edit/', methods=['GET', 'POST'])
 def editItem(item_id):
-    editItem = session.query(Item).filter_by(id=item_id).first()
+    editItem = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editItem.name = request.form['name']
-            return redirect(url_for('main.showAll'))
+            return redirect(url_for('posts.showAll'))
     else:
         return render_template('editItem.html', item = editItem)
 
 # Delete route to delete item names
 @posts.route('/items/<int:item_id>/delete/', methods=['GET', 'POST'])
 def deleteItem(item_id):
-    itemDelete = session.query(Item).filter_by(id=item_id).first()
+    itemDelete = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
         session.delete(itemDelete)
         session.commit()
-        return redirect(url_for('main.showAll', item_id = item_id))
+        return redirect(url_for('posts.showAll', item_id = item_id))
     else:
         return render_template('editItem.html', itemDelete = itemDelete)
 
@@ -58,14 +58,14 @@ def deleteItem(item_id):
 @posts.route('/items/<int:item_id>/')
 @posts.route('/items/<int:item_id>/details/')
 def showItem(item_id):
-    item = session.query(Item).filter_by(id=item_id).first()
+    item = session.query(Item).filter_by(id=item_id).one()
     itemDetails = session.query(ItemCategory).filter_by(item_id = item_id).all()
     return render_template('details.html', itemDetails=itemDetails, item=item)
 
 # Create new details for an item
 @posts.route('/items/<int:item_id>/details/new/', methods=['GET', 'POST'])
 def newCategory(item_id):
-    itemName = session.query(Item).filter_by(id=item_id).first()
+    itemName = session.query(Item).filter_by(id=item_id).one()
 
     if request.method == 'POST':
         newItem = ItemCategory(description=request.form['description'], category=request.form['category'], name=request.form['name'], price=request.form['price'], stock=request.form['stock'], item_id=item_id)
@@ -81,8 +81,8 @@ def newCategory(item_id):
 # Edit details for a catalog item
 @posts.route('/items/<int:item_id>/details/<int:itemDetails_id>/edit', methods=['GET', 'POST'])
 def editDetails(item_id, itemDetails_id):
-    itemName = session.query(Item).filter_by(id=item_id).first()
-    editedItem = session.query(ItemCategory).filter_by(id=item_id).first()
+    itemName = session.query(Item).filter_by(id=item_id).one()
+    editedItem = session.query(ItemCategory).filter_by(id=item_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editedItem.name = request.form['name']
@@ -104,8 +104,8 @@ def editDetails(item_id, itemDetails_id):
 # Delete details for a catalog item
 @posts.route('/items/<int:item_id>/details/<int:itemDetails_id>/delete', methods=['GET', 'POST'])
 def deleteDetails(item_id, itemDetails_id):
-    itemToDelete = session.query(ItemCategory).filter_by(id=item_id).first()
-    item = session.query(Item).filter_by(id=item_id).first()
+    itemToDelete = session.query(ItemCategory).filter_by(id=item_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
